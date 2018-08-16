f8abc6e9966a91052cbdc12b954209323de27a5d
闪店zs <sd-1@shandiandeMini2.lan>
2018-08-16 14:49:06 +0800

    新增批量调度路由

--- src/config/routes.js
+++ src/config/routes.js
@@ -84,6 +84,17 @@ const processMaterialPrint = (resolve) =>
 const processMaterial = (resolve) =>
     import (/*webpackChunkName:'process_material'*/'src/module/invoicing_system/process/process_material');                 //加工物料
 
+
+//批量调度加工日志
+const batchLogDispatch = (resolve) =>
+    import (/*webpackChunkName:'batch_log_dispatch'*/'src/module/invoicing_system/batch_log/batch_log_dispatch');         //批量调度,审核日志
+const batchLogDispatchDetail = (resolve) =>
+    import (/*webpackChunkName:'batch_log_dispatch_detail'*/'src/module/invoicing_system/batch_log/batch_log_dispatch_detail');  //批量调度日志详情
+const batchLogDetail = (resolve) =>
+    import (/*webpackChunkName:'batch_log_detail'*/'src/module/invoicing_system/batch_log/batch_log_detail');  //批量调度,审核日志详情
+const batchLogPrint = (resolve) =>
+    import (/*webpackChunkName:'batch_log_print'*/'src/module/invoicing_system/batch_log/batch_log_print');  //批量调度,审核日志详情打印
+
 const suppliesDetail = (resolve) =>
     import ( /* webpackChunkName:"supplies_detail" */ 'src/module/invoicing_system/invoicing/supplies_detail'); //进销存详情物料详情   
 const putstroage = (resolve) =>
@@ -1666,15 +1677,70 @@ export default [{
                         details_con: goodsCount //商品盘库  
                     }
                 },{
-                    path:'processMaterialPrint',
+                    path:'processMaterialPrint',                    //加工物料打印
                     components:{
                         details_con: processMaterialPrint
                     }
-                },{
+                },
+                {
                     path:'processMaterial',
                     components:{
-                        details_con: processMaterial
+                        details_con: processMaterial                //加工物料
                     }
+                },
+                //批量调度日志
+                {
+                    path:'batchLog',                        
+                    components:{
+                        details_con: details_con
+                    },
+                    children:[
+                        {
+                            path:'',
+                            redirect:'batchLogDispatch'                            
+                        },
+                        {
+                            path:'batchLogDispatch',
+                            components:{
+                                details_con:batchLogDispatch                //批量调度日志列表      
+                            },
+                            alias:'',                              
+                            props:{
+                                details_con:(route) => {
+                                    return { tab: 1};
+                                }
+                            }
+                        },
+                        {
+                            path:'batchLogAudit',
+                            components:{
+                                details_con:batchLogDispatch                //批量审核日志列表      
+                            },    
+                            props:{
+                                details_con:(route) => {
+                                    return { tab: 2};
+                                }
+                            }                          
+                        },
+                        {
+                            path:'batchLogPrint',
+                            components:{
+                                details_con: batchLogPrint                  //批量审核详情打印
+                            }
+                        },
+                        {
+                            path:'batchLogDetail',
+                            components:{
+                                details_con: batchLogDetail                  //批量审核日志详情
+                            }
+                        },
+                        {
+                            path:'batchLogDispatchDetail',
+                            components:{
+                                details_con: batchLogDispatchDetail          //批量调度日志详情
+                            }
+                        },
+                    ]
                 },
                 {
                     path: 'materialCount', //批量盘库-物料盘库
