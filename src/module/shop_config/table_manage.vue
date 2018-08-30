25d150558a462f312f0c564ff1313c098ba1995f
hujiang <jiang.hu@ishandian.net>
2018-08-30 15:27:29 +0800

    桌台列表化

--- src/module/shop_config/table_manage.vue
+++ src/module/shop_config/table_manage.vue
@@ -7,46 +7,76 @@
 -->
 <template>
 	<div id="table-show">
-		<ul class="tas-variety" v-cloak>
-			<li v-on:click="add">
-				<span>
-					<img src="../../res/images/comadd.png" alt="添加" style="margin-top: 30px;opacity:0.5;" />
-				</span>
-				新建桌台
-			</li>
-			<li v-for="(item,i) in tableList" class="taste-list" v-on:click="edit(item.id,i)" :key="i">
-				<div class="divOne"></div>
-				<div class="divThree">
-					<span class="spanName" :class="{'width200':i+1<10,'width180':(i+1)<100&&(i+1)>=10,'width140':i+1>=100}">{{item.areaName}} - {{item.tableName}}</span>
-					<span class="spanIndex">{{i+1}}</span>
-				</div>
-				<div class="divFro">
-					<span> 默认席位：{{item.normalSeat}}</span>
-				</div>
-				<div class="divTwo">
-					<div class="jobDiv">
-						<div>
-							最少容纳(位)
-						</div>
-						<div class="divAll">
-							{{item.minSeat}}
-						</div>
-					</div>
-					<div class="telDiv">
-						<div>
-							最多容纳（位）
-						</div>
-						<div class="divAll">
-							{{item.maxSeat}}
-						</div>
-						<section class="table-code" @click="doji($event)">
-							<a :href="host+'table/createTableCode?format=json&tableId=' + item.id +'&shopId=' + userData.currentShop.id+ '&areaName='+item.areaName+'&tableName='+item.tableName+'&token='+userData.accessToken"></a>
-						</section>
-					</div>
-				</div>
-			</li>
-		</ul>
-		<tableWin v-if="showWin" @throwWinResult="doThrowWinResult" :isAdd="isAdd" :tableDetial='tableDetial' :tableId="tableId" :tableIndex="tableIndex" :userData="userData" :tableList="tableList" :Area="Area">
+		<section>
+			<el-table ref="multipleTable" stripe :header-cell-style = "{'background-color':'#f5f7fa'}" :data="showTableList" border style="width:90%">
+				<el-table-column show-overflow-tooltip min-width = "80" align="center" label="操作">
+					<template slot-scope="scope">
+						<span style="color: #FE8D2C;cursor:pointer" @click="edit(scope.row)">编辑</span>
+					</template>
+				</el-table-column>
+				<el-table-column show-overflow-tooltip min-width = "80" align="center" prop="sort" label="排序"></el-table-column>
+				<el-table-column show-overflow-tooltip min-width = "120"  align="center" prop="tableName"  label="桌台名称"> </el-table-column>
+				<el-table-column show-overflow-tooltip min-width = "120" align="center" prop="areaName" label="区域名称"></el-table-column>
+				<el-table-column show-overflow-tooltip min-width = "80"  align="center" prop="normalSeat"  label="默认席位"> </el-table-column>
+				<el-table-column show-overflow-tooltip min-width = "80"  align="center" prop="minSeat"  label="最少容纳(位)"> </el-table-column>
+				<el-table-column show-overflow-tooltip min-width = "80"  align="center" prop="maxSeat"  label="最多容纳(位)"> </el-table-column>
+				<el-table-column  min-width="80" align="center" label="二维码">
+					<template slot-scope="scope">
+						<span class="table-code">
+							<a :href="host+'table/createTableCode?format=json&tableId=' + scope.row.id +'&shopId=' + userData.currentShop.id+ '&areaName='+scope.row.areaName+'&tableName='+scope.row.tableName+'&token='+userData.accessToken"></a>
+						</span>
+					</template>
+				</el-table-column>
+			</el-table>
+		</section>
+		<section style="margin-top: 10px">
+			<el-pagination background @size-change="numChange" @current-change="pageClick" :current-page="page" :page-size = "num"
+						   layout="sizes, prev, pager, next" :page-count="total" :page-sizes="[10, 20, 30]"></el-pagination>
+		</section>
+
+
+		<!--<ul class="tas-variety" v-cloak>-->
+			<!--<li v-on:click="add">-->
+				<!--<span>-->
+					<!--<img src="../../res/images/comadd.png" alt="添加" style="margin-top: 30px;opacity:0.5;" />-->
+				<!--</span>-->
+				<!--新建桌台-->
+			<!--</li>-->
+			<!--<li v-for="(item,i) in tableList" class="taste-list" v-on:click="edit(item.id,i)" :key="i">-->
+				<!--<div class="divOne"></div>-->
+				<!--<div class="divThree">-->
+					<!--<span class="spanName" :class="{'width200':i+1<10,'width180':(i+1)<100&&(i+1)>=10,'width140':i+1>=100}">{{item.areaName}} - {{item.tableName}}</span>-->
+					<!--<span class="spanIndex">{{i+1}}</span>-->
+				<!--</div>-->
+				<!--<div class="divFro">-->
+					<!--<span> 默认席位：{{item.normalSeat}}</span>-->
+				<!--</div>-->
+				<!--<div class="divTwo">-->
+					<!--<div class="jobDiv">-->
+						<!--<div>-->
+							<!--最少容纳(位)-->
+						<!--</div>-->
+						<!--<div class="divAll">-->
+							<!--{{item.minSeat}}-->
+						<!--</div>-->
+					<!--</div>-->
+					<!--<div class="telDiv">-->
+						<!--<div>-->
+							<!--最多容纳（位）-->
+						<!--</div>-->
+						<!--<div class="divAll">-->
+							<!--{{item.maxSeat}}-->
+						<!--</div>-->
+						<!--<section class="table-code" @click="doji($event)">-->
+							<!--<a :href="host+'table/createTableCode?format=json&tableId=' + item.id +'&shopId=' + userData.currentShop.id+ '&areaName='+item.areaName+'&tableName='+item.tableName+'&token='+userData.accessToken"></a>-->
+						<!--</section>-->
+					<!--</div>-->
+				<!--</div>-->
+			<!--</li>-->
+		<!--</ul>-->
+
+		<tableWin v-if="showWin" @throwWinResult="doThrowWinResult" :isAdd="isAdd"  :tableId="tableId"
+				   :userData="userData" :tableDetial="tableDetial" :Area="Area">
 		</tableWin>
 	</div>
 </template>
@@ -62,23 +92,45 @@ export default {
 		return {
 			host: global.host.shop,
 			Area: [], //区域列表
-			tableList: Array, //桌台列表
+			tableList:[], //桌台列表
 			userData: Object,
 			isAdd: true, //是否是添加，true为是
 			showWin: false,
 			tableId: '', //桌台id
-			tableIndex: '' //桌台索引
+
+			tableDetial:null,//桌台详细信息
+
+			num: 10, //一页处理多少数据
+			total: 0, //总页数
+			page: 1, //当前第几页
+			showTableList:[],//展示的桌台列表
 		};
 	},
-	mounted() {
+	created(){
+		let arr = [{name:'新建桌台',className:'pick',fn:()=>{
+			this.add();
+		}}];
+		this.$store.commit('setPageTools',arr);
 		this.userData = storage.session('userShop');
 		this.init();
 		this.getArea();
 	},
 	methods: {
-		//阻止事件冒泡
-		doji(e) {
-			e.stopPropagation();
+		//分页
+		paging() {
+			this.total = Math.ceil(this.tableList.length / this.num); //获取总页数
+			this.showTableList = this.tableList.slice((this.page - 1) * this.num, (this.page - 1) * this.num + this.num
+			); //截取当前页数据
+		},
+		//每页显示几条数据
+		numChange(e){
+			this.num=e;
+			this.paging();
+		},
+		//切换到第几页
+		pageClick(e){
+			this.page=e;
+			this.paging();
 		},
 		//桌台列表
 		async init() {
@@ -90,6 +142,7 @@ export default {
 			if (res) {
 				this.tableList = res;
 				this.sortList();
+				this.paging();
 			}
 		},
 		//得到区域列表
@@ -109,25 +162,47 @@ export default {
 			this.showWin = true;
 		},
 		//编辑
-		async edit(id, index) {
+		async edit(item) {
+			console.log(item);
 			this.isAdd = false;
-			this.tableId = id;
-			this.tableIndex = index;
-			this.tableDetial = await http.getTableInfo({
-				data: {
-					shopId: this.userData.currentShop.id,
-					id: this.tableId
-				}
-			});
+			this.tableId = item.id;
+			this.tableDetial=utils.deepCopy(item);
+			this.tableDetial.id=this.tableDetial.id+'';
+//			this.tableDetial = await http.getTableInfo({
+//				data: {
+//					shopId: this.userData.currentShop.id,
+//					id: this.tableId
+//				}
+//			});
 			this.showWin = true;
 		},
-		doThrowWinResult(res) {
+		doThrowWinResult(res,data) {
+			console.log(data);
 			if (res == 'ok') {
+				if(this.isAdd){ //添加
+					for (let i = 0; i < data.length; i++) {
+						this.tableList.push(data[i]);
+					}
+				}else { //编辑
+					for(let j=0;j<this.tableList.length;j++){
+						if(this.tableList[j].id==this.tableId){
+							this.tableList.splice(j,1,data);
+							break;
+						}
+					}
+				}
 				this.sortList();
+				this.paging();
 				this.showWin = false;
 			} else if (res == 'cancel') {
-				if (!this.isAdd) {
-					this.sortList();
+				if (!this.isAdd) { //删除
+					for(let j=0;j<this.tableList.length;j++){
+						if(this.tableList[j].id==data){
+							this.tableList.splice(j,1);
+							break;
+						}
+					}
+					this.paging();
 				}
 				this.showWin = false;
 			} else if (res == 'close') {
@@ -152,7 +227,7 @@ export default {
 
 <style scoped lang="less">
 #table-show {
-	padding-top: 30px;
+	/*padding-top: 30px;*/
 	ul.tas-variety {
 		li {
 			&:first-child {
@@ -258,6 +333,20 @@ export default {
 					font-size: 18px;
 				}
 			}
+		}
+	}
+	.table-code {
+		width: 30px;
+		height: 30px;
+		background-image: url(../../res/images/code.png);
+		background-size: 30px 30px;
+		display: inline-block;
+		a {
+		width: 100%;
+		height: 100%;
+		}
+		&:hover {
+			background-image: url(../../res/images/downcode.png);
 		}
 	}
 }
