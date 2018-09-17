6afc6394b011c3460338a82e29531e8ecccdccf9
ruixinMRK <ruokun.ma@ishandian.net>
2018-09-17 10:52:04 +0800

    修改eslint规范

--- src/config/cateroutes.js
+++ src/config/cateroutes.js
@@ -8,27 +8,27 @@ var Login = 'Login';
 var con = 'con';
 var details_con = 'details_con';
 while (res != null) {
-    let obj = {};
-    let st = res[0].split(' ');
-    global[st[1]] = st[1];
-    let firreg = /(src).+\w/;
-    firreg.exec(res[0]);
-    let firstr = firreg.exec(res[0])[0];
-    let firarr = firreg.exec(res[0])[0].split('/');
-    // console.log(firarr);
-    Object.assign(obj, {
-        name: st[1],
-        string: res[0],
-        firstr: firstr,
-        firName: firarr[2],
-        lastfirName: firarr[firarr.length - 1],
-        cont: 0
-    })
-    impArr.push(obj);
-    res = reg.exec(data);
+	let obj = {};
+	let st = res[0].split(' ');
+	global[st[1]] = st[1];
+	let firreg = /(src).+\w/;
+	firreg.exec(res[0]);
+	let firstr = firreg.exec(res[0])[0];
+	let firarr = firreg.exec(res[0])[0].split('/');
+	// console.log(firarr);
+	Object.assign(obj, {
+		name: st[1],
+		string: res[0],
+		firstr: firstr,
+		firName: firarr[2],
+		lastfirName: firarr[firarr.length - 1],
+		cont: 0
+	})
+	impArr.push(obj);
+	res = reg.exec(data);
 }
 if (!fs.existsSync('routes')) {
-    fs.mkdirSync('routes');
+	fs.mkdirSync('routes');
 }
 // let arrreg = /(export([\w\W]*))([\w\W]*)\]/;
 let arrreg = /\[([\w\W]*)\]/;
@@ -54,85 +54,85 @@ rtarr = eval(rtarr[0]);
 //     return childarr;
 // }
 let config = {};
-let senFlie = function (item) {
-    if (item.children) {
-        let child = [];
-        item.children.forEach(element => {
-            child.push(senFlie(element));
-        });
-        item.children = child;
-    }
-    if (item.components && item.components.details_con != 'details_con') {
-        let fir = getFile(item.components.details_con);
-        item.url = `${fir.firstr}`;
-    }
-    delete item.components;
-    return item;
+let senFlie = function(item) {
+	if (item.children) {
+		let child = [];
+		item.children.forEach(element => {
+			child.push(senFlie(element));
+		});
+		item.children = child;
+	}
+	if (item.components && item.components.details_con != 'details_con') {
+		let fir = getFile(item.components.details_con);
+		item.url = `${fir.firstr}`;
+	}
+	delete item.components;
+	return item;
 }
-let getFile = function (name, type) {
-    let need = '';
-    for (let fir of impArr) {
-        if (name == fir.name) {
-            need = fir;
-            if (fir.cont >= 1 && type) {
-                fir.lastfirName += 's';
-            }
-            fir.cont++;
-            break;
-        }
-    }
-    return need
+let getFile = function(name, type) {
+	let need = '';
+	for (let fir of impArr) {
+		if (name == fir.name) {
+			need = fir;
+			if (fir.cont >= 1 && type) {
+				fir.lastfirName += 's';
+			}
+			fir.cont++;
+			break;
+		}
+	}
+	return need
 }
 for (let item of rtarr[3].children[0].children) {
-    let fir = '';
-    item = setfuncton(item);
-    if (item.components && item.components.details_con != 'details_con') {
-        fir = getFile(item.components.details_con, true);
-    } else {
-        for (let v of item.children) {
-            if (v.components && v.components.details_con != 'details_con') {
-                fir = getFile(v.components.details_con, true);
-                break;
-            }
-        }
-    }
-    if (!fs.existsSync(`./routes/${fir.firName}`)) { //获取模块列表对象
-        fs.mkdirSync(`./routes/${fir.firName}`);
-    }
-    if (!config[fir.firName]) {
-        config[fir.firName] = {};
-    }
-    config[fir.firName][fir.lastfirName] = fir.lastfirName;
-    let writestr = `module.exports=${JSON.stringify(senFlie(item))}`;
-    writestr = writestr.replace(/\$"|"\$|\\r|\\n/g, ''); //去除分号
-    fs.writeFileSync(`./routes/${fir.firName}/${fir.lastfirName}.js`, writestr);
+	let fir = '';
+	item = setfuncton(item);
+	if (item.components && item.components.details_con != 'details_con') {
+		fir = getFile(item.components.details_con, true);
+	} else {
+		for (let v of item.children) {
+			if (v.components && v.components.details_con != 'details_con') {
+				fir = getFile(v.components.details_con, true);
+				break;
+			}
+		}
+	}
+	if (!fs.existsSync(`./routes/${fir.firName}`)) { //获取模块列表对象
+		fs.mkdirSync(`./routes/${fir.firName}`);
+	}
+	if (!config[fir.firName]) {
+		config[fir.firName] = {};
+	}
+	config[fir.firName][fir.lastfirName] = fir.lastfirName;
+	let writestr = `module.exports=${JSON.stringify(senFlie(item))}`;
+	writestr = writestr.replace(/\$"|"\$|\\r|\\n/g, ''); //去除分号
+	fs.writeFileSync(`./routes/${fir.firName}/${fir.lastfirName}.js`, writestr);
 }
 
 function setfuncton(item) { //处理对象中的函数
-    for (let i in item) {
-        if (typeof item[i] == "object") {
-            item[i] = setfuncton(item[i]);
-        } else if (typeof item[i] == "function") {
-            item[i] = `$${item[i]}$`
-        }
-    }
-    return item
+	for (let i in item) {
+		if (typeof item[i] == "object") {
+			item[i] = setfuncton(item[i]);
+		} else if (typeof item[i] == "function") {
+			item[i] = `$${item[i]}$`
+		}
+	}
+	return item
 }
 let newconfig = {};
 let type = 1;
 let cofStr = ''
 for (let key in config) {
-    let last = 1;
-    let tem = '';
-    let temobj = {};
-    // newconfig[key] = {};
-    for (let s in config[key]) {
-        temobj[`${type<10?0:''}${type}${last}`] = key + '/' + config[key][s];
-        last++;
-    }
-    tem = `const ${key} = ${JSON.stringify(temobj)};\n`;
-    cofStr += tem
-    type++;
+	let last = 1;
+	let tem = '';
+	let temobj = {};
+	// newconfig[key] = {};
+	for (let s in config[key]) {
+		temobj[`${type<10?0:''}${type}${last}`] = key + '/' + config[key][s];
+		last++;
+	}
+	tem = `const ${key} = ${JSON.stringify(temobj)};\n`;
+	cofStr += tem
+	type++;
 }
 let addexp = `let config = {useId: [], //需要用到的路由id
                     configs: {} //需要插入的路由模块
