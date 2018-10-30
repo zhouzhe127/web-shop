1870df3ceb3d51ac538d1d475a42bfb1dd27d65a
闪店zs <sd-1@shandiandeMini2.lan>
2018-10-30 15:05:04 +0800

    添加路由

--- src/config/routes/invoicing_system/material_report_template.js
+++ src/config/routes/invoicing_system/material_report_template.js
@@ -1,11 +1,22 @@
 /* eslint-disable */
 module.exports = {
-    "path": "dataCenter",
-    "children": [{
-        "path": "reportTemplate",
-        "meta": {
-            // "keepAlive": true
+    "path": "materialReport",
+    "children": [
+        {
+            "path": "",
+            "meta": {
+                // "keepAlive": true
+            },
+            "url": "src/module/invoicing_system/data_center/material_report_template"
         },
-        "url": "src/module/invoicing_system/data_center/material_report_template"
-    }]
+        {
+            path:'finishedReport',
+            url:'src/module/invoicing_system/data_center/material_finished_report'          //已生成报表
+        },
+        {
+            path:'viewReport',
+            url:'src/module/invoicing_system/data_center/material_view_report'          //已生成报表
+        }
+
+    ]
 }
