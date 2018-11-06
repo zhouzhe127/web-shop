3ec341605902b3c3a9df9d98c1c7959b0cad30e7
mxc <819929529@qq.com>
2018-11-06 16:15:56 +0800

    新建工作人员

--- src/config/routes/seller_assistant/worker_staff.js
+++ src/config/routes/seller_assistant/worker_staff.js
@@ -1,2 +1,2 @@
 /* eslint-disable */
-module.exports={"path":"Assistantstaff","url":"src/module/seller_assistant/worker_staff"}
+module.exports={"path":"Assistantstaff","url":"src/module/seller_assistant/staff/worker_staff"}
