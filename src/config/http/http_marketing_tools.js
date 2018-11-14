f2c959553fff0c8564df85d2e7f7682f8bbc508d
zhigang.li <zhigang.li@ishandian.net>
2018-11-14 20:43:41 +0800

    api

--- src/config/http/http_marketing_tools.js
+++ src/config/http/http_marketing_tools.js
@@ -360,39 +360,39 @@ export default {
 	/* ↓ 砍价活动(粮票集) ↓
 	-------------------*/
 	'activityCreateActivity': {
-		url: f + 'Activity/createActivity', //新建砍价活动
+		url: f + 'Api/Activity/createActivity', //新建砍价活动
 		method: 'post'
 	},
 	'activityEditActivity': {
-		url: f + 'Activity/editActivity', // 修改砍价活动
+		url: f + 'Api/Activity/editActivity', // 修改砍价活动
 		method: 'post'
 	},
 	'activityAddGoods': {
-		url: f + 'Activity/addGoods', //添加活动商品
+		url: f + 'Api/Activity/addGoods', //添加活动商品
 		method: 'post'
 	},
 	'activityEditGoods': {
-		url: f + 'Activity/editGoods', // 编辑活动商品
+		url: f + 'Api/Activity/editGoods', // 编辑活动商品
 		method: 'post'
 	},
 	'activityPushGoods': {
-		url: f + 'Activity/pushGoods', // 上架活动商品
+		url: f + 'Api/Activity/pushGoods', // 上架活动商品
 		method: 'post'
 	},
 	'activityRevokePushGoods': {
-		url: f + 'Activity/revokePushGoods', // 下架活动商品
+		url: f + 'Api/Activity/revokePushGoods', // 下架活动商品
 		method: 'post'
 	},
 	'statisticGetActivity': {
-		url: f + 'Statistic/getActivity', // 获取砍价活动列表
+		url: f + 'Api/Statistic/getActivity', // 获取砍价活动列表
 		method: 'get'
 	},
 	'activityGetActivityDetail': {
-		url: f + 'activity/getActivityDetail', // 获取 活动统计明细
+		url: f + 'Api/activity/getActivityDetail', // 获取 活动统计明细
 		method: 'get'
 	},
 	'statisticGetActivityGoods': {
-		url: f + 'Statistic/getActivityGoods', // 获取砍价活动明细
+		url: f + 'Api/Statistic/getActivityGoods', // 获取砍价活动明细
 		method: 'get'
 	},
 	/* ↑ 砍价活动(粮票集) ↑
