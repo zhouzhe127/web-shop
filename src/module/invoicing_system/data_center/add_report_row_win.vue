119990ca1b412f898aec37c199a5af9d9ec66698
weiyan.kong <weiyan.kong@ishandian.net>
2018-12-20 17:15:51 +0800

    添加行文字调整

--- src/module/invoicing_system/data_center/add_report_row_win.vue
+++ src/module/invoicing_system/data_center/add_report_row_win.vue
@@ -1,8 +1,8 @@
 /*
  * @Author: weifu.zeng 
  * @Date: 2018-11-02 11:19:44 
- * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-12-19 09:48:38
+ * @Last Modified by: 孔伟研
+ * @Last Modified time: 2018-12-20 17:15:07
  */
 <template>  
 	<div>
@@ -30,8 +30,8 @@
 				<div class="textarea">
 					<div v-if="collection.id">
 						<p class="conllection-description">集合名称 : {{collection.name}}</p>
-						<p class="conllection-description">集合类型 : {{collection.instruction}}</p>
-						<p class="conllection-description">集合说明 : {{collection.typeName}}</p>
+						<p class="conllection-description">集合类型 : {{collection.typeName}}</p>
+						<p class="conllection-description">集合说明 : {{collection.instruction}}</p>
 					</div>
 				</div>
 
